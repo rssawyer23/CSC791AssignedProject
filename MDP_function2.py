679f3ebb858e0f769ee573a2e84b2c361836c00b
Rob Sawyer <robscottsawyer@gmail.com>
2017-03-31 13:41:32 -0400

    PCA Feature Selection Function
    NOTE CHANGES MADE TO THE "2" FILES. Added a PCA Feature Selection which
    uses all of the data to create a given number of features with a given
    number of discretization levels. Not working as well as I had hoped on
    its own, will have to make modifications.

--- MDP_function2.py
+++ MDP_function2.py
@@ -4,6 +4,7 @@ import pandas
 import mdptoolbox, mdptoolbox.example
 import argparse
 import sklearn.decomposition
+from scipy import stats
 
 
 def generate_MDP_input2(original_data, features):
@@ -11,8 +12,8 @@ def generate_MDP_input2(original_data, features):
     students_variables = ['student', 'priorTutorAction', 'reward']
 
     # generate distinct state based on feature
-    #original_data['state'] = original_data[features].apply(lambda x: ':'.join(str(v) for v in x), axis=1)
-    original_data['state'] = original_data[features].apply(tuple, axis=1)
+    original_data['state'] = original_data[features].apply(lambda x: ':'.join(str(v) for v in x), axis=1)
+    # original_data['state'] = original_data[features].apply(tuple, axis=1)
     students_variables = students_variables + ['state']
     data = original_data[students_variables]
 
@@ -25,15 +26,25 @@ def generate_MDP_input2(original_data, features):
         i += 1
 
     # initialize state transition table, expected reward table, starting state table
-    distinct_states = list(data['state'].unique())
+    # distinct_states didn't contain terminal state
+    student_list = list(data['student'].unique())
+    distinct_states = list()
+    for student in student_list:
+        student_data = data.loc[data['student'] == student,]
+        # don't consider last row
+        temp_states = list(student_data['state'])[0:-1]
+        distinct_states = distinct_states + temp_states
+    distinct_states = list(set(distinct_states))
+
     Ns = len(distinct_states)
-    start_states = np.zeros(Ns)
-    A = np.zeros((Nx, Ns, Ns))
-    expectR = np.zeros((Nx, Ns, Ns))
+
+    # we include terminal state
+    start_states = np.zeros(Ns + 1)
+    A = np.zeros((Nx, Ns+1, Ns+1))
+    expectR = np.zeros((Nx, Ns+1, Ns+1))
 
     # update table values episode by episode
     # each episode is a student data set
-    student_list = list(data['student'].unique())
     for student in student_list:
         student_data = data.loc[data['student'] == student,]
         row_list = student_data.index.tolist()
@@ -41,7 +52,8 @@ def generate_MDP_input2(original_data, features):
         # count the number of start state
         start_states[distinct_states.index(student_data.loc[row_list[0], 'state'])] += 1
 
-        for i in range(1, len(row_list)):
+        # count the number of transition among states without terminal state
+        for i in range(1, (len(row_list)-1)):
             state1 = distinct_states.index(student_data.loc[row_list[i - 1], 'state'])
             state2 = distinct_states.index(student_data.loc[row_list[i], 'state'])
             act = student_data.loc[row_list[i], 'priorTutorAction']
@@ -50,19 +62,27 @@ def generate_MDP_input2(original_data, features):
             A[act, state1, state2] += 1
             expectR[act, state1, state2] += float(student_data.loc[row_list[i], 'reward'])
 
+        # count the number of transition from state to terminal
+        state1 = distinct_states.index(student_data.loc[row_list[-2], 'state'])
+        act = student_data.loc[row_list[-1], 'priorTutorAction']
+        A[act, state1, Ns] += 1
+        expectR[act, state1, Ns] += float(student_data.loc[row_list[-1], 'reward'])
+
     # normalization
     start_states = start_states / np.sum(start_states)
 
     for act in range(Nx):
+        A[act, Ns, Ns] = 1
         # generate expected reward
-        # it has the warning, ignore it
         with np.errstate(divide='ignore', invalid='ignore'):
             expectR[act] = np.divide(expectR[act], A[act])
             expectR[np.isnan(expectR)] = 0
 
         # each column will sum to 1 for each row, obtain the state transition table
+        # some states only have either PS or WE transition to other state
         for l in np.where(np.sum(A[act], axis=1) == 0)[0]:
-            A[act][l][l] = 1
+            A[act, l, l] = 1
+            
         A[act] = np.divide(A[act].transpose(), np.sum(A[act], axis=1))
         A[act] = A[act].transpose()
 
@@ -79,8 +99,7 @@ def output_policy(distinct_acts, distinct_states, vi):
     print('Policy: ')
     print('state -> action, value-function')
     for s in range(Ns):
-        print str(distinct_states[s]) + " -> " + str(distinct_acts[vi.policy[s]]) + ", " + str(vi.V[s])
-
+        print(distinct_states[s] + " -> " + distinct_acts[vi.policy[s]] + ", " + str(vi.V[s]))
 
 def induce_policy_MDP2(original_data, selected_features):
 
@@ -95,15 +114,14 @@ def induce_policy_MDP2(original_data, selected_features):
 
     # evaluate policy using ECR
     ECR_value = calcuate_ECR(start_states, vi.V)
-    print 'ECR value: %.5f' % ECR_value
+    print('ECR value: ' + str(ECR_value))
     return ECR_value
-
 
 # Function implemented by Rob for discretizing a column
 # data_series is continuous valued column to be discretized
 # bins is the number of "levels" for discretization (default 2 = binary)
 def discretize_column(data_series, bins=2):
-    sorted_series = data_series.order(ascending=True, inplace=False)
+    sorted_series = data_series.sort_values(ascending=True, inplace=False)
     return_series = pandas.Series([0]*data_series.shape[0], dtype=int)
     for i in range(1, bins):
         split = sorted_series.iloc[sorted_series.shape[0]*i/bins]
@@ -130,19 +148,32 @@ def testing_feature_selection(data_frame):
     return feature_list, data_frame
 
 
-# Function for doing feature selection by PCA
-def PCA_feature_selection(data_frame, vectors_used=8):
-    feature_list = []
+
+# Function for performing feature selection via Principal Component Analysis (dimensionality reduction)
+# vectors_used is number of component vectors (resulting features) to use
+# bins is the number of levels to discretize each resulting feature vector into
+# uniform controls whether each feature vector has the same amount of levels or whether level amount should decrease (less important vectors later)
+def PCA_feature_selection(data_frame, vectors_used=8, bins=2, uniform=True):
     pca = sklearn.decomposition.PCA(n_components=vectors_used)
-    pca.fit(data_frame.iloc[, 5:])
-    print pca.explained_variance_ratio_
-    return feature_list, None
+    transformed_data = pca.fit_transform(data_frame.iloc[:, 5:])
+    feature_list = ["New-Feature-%d" % i for i in range(vectors_used)]
+    extra_data = pandas.DataFrame(transformed_data, columns=feature_list)
+    result_data = pandas.concat([data_frame, extra_data], axis=1)
+    if uniform:
+        for f in feature_list:
+            result_data[f] = discretize_column(result_data[f], bins)
+    else:
+        for f,i in zip(feature_list,range(len(feature_list))):
+            new_bins = max(bins - i, 2)
+            result_data[f] = discretize_column(result_data[f], new_bins)
+    return feature_list, result_data
+
 
 
 if __name__ == "__main__":
 
     original_data = pandas.read_csv('MDP_Original_data2.csv')
 
-    selected_features, expanded_data = PCA_feature_selection(original_data)
+    selected_features, expanded_data = PCA_feature_selection(original_data, vectors_used=8, bins=4, uniform=False)
 
-    #ECR_value = induce_policy_MDP2(expanded_data, selected_features)
+    ECR_value = induce_policy_MDP2(expanded_data, selected_features)
